e6d7c0886ca8e756da8511ed576112b299da846d
Valentine <zavrak.valentin@gmail.com>
2017-10-22 18:31:37 +0300

    user login and JWT were added

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 
 router.post('/api/users/create-user', usersController.createUser);
+router.post('/api/users/login-user', usersController.loginUser);
 
 module.exports = router;
